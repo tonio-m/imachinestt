efa4c1e07f966088c78539c8407c0b5e41bab021
tonio-m <anntonio@protonmail.ch>
2022-05-01 16:56:14 -0300

    fixing wrong column name on create table

--- utils/create_table.sql
+++ utils/create_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE captcha
     site_id UUID,
     time DateTime,
     correlation_id UUID,
-    event_type String
-) ENGINE = MergeTree ORDER BY (time,site_id,correlation_id,event_type)
+    type String
+) ENGINE = MergeTree ORDER BY (time,site_id,correlation_id,type)
 
 
 CREATE TABLE default.captcha_queue
@@ -12,7 +12,7 @@ CREATE TABLE default.captcha_queue
     site_id UUID,
     time DateTime,
     correlation_id UUID,
-    event_type String
+    type String
 )
    ENGINE = Kafka('kafka.confluent.svc.cluster.local:9071', 'captcha', 'clickhouse',
             'JSONEachRow') settings kafka_thread_per_consumer = 0, kafka_num_consumers = 1;
